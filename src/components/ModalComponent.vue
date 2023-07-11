978ee00af0ea1982fc847c930379ff9b15227f7c
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-07-11 01:26:30 -0500

    se agrego teleport al modal

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="content-modal">
     <button @click="toggle">Modal</button>
-    <div v-show="show" class="modal">
-      <h1>titulo</h1>
-      <p>lorem ipsum</p>
-      <button @click="toggle">cerrar</button>
-    </div>
+    <teleport to="body">
+      <div v-show="show" class="modal">
+        <h1>titulo</h1>
+        <p>lorem ipsum</p>
+        <button @click="toggle">cerrar</button>
+      </div>
+    </teleport>
   </div>
 </template>
 
@@ -23,3 +25,19 @@ export default {
   },
 };
 </script>
+<style>
+.content-modal {
+  width: 100vw;
+  height: 100vh;
+}
+.modal {
+  width: 100%;
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+h1 {
+  text-transform: uppercase;
+}
+</style>
