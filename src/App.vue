c69d2083d68955507617aefbd04b7cecd499511c
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-07-11 00:12:17 -0500

    inicio de clase teleport

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
-  <button @click="show = !show">MENU</button>
-  <transition name="fade">
-    <MainMenu v-show="show" />
-  </transition>
+  <h1>titulo</h1>
 </template>
 
 <script>
-import MainMenu from "./components/MainMenu.vue";
 export default {
   name: "App",
-  components: { MainMenu },
-  data() {
-    return {
-      show: false,
-    };
-  },
+  components: {},
 };
 </script>
 
@@ -26,15 +17,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
 }
 </style>
