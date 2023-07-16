2ce4e43df27309850db195e41a6cd2fc22927917
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-07-16 01:48:05 -0500

    funcion toggle para cambiar el valor de la referencia de valor booleano

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>{{ text }}</div>
+  <div>{{ encendido ? "abierto" : "cerrado" }}</div>
+  <button v-on:click="toggle">encender/ apagar</button>
 </template>
 
 <script>
 import { ref } from "vue";
 export default {
   setup() {
-    const text = ref("hola Vue");
+    const encendido = ref(true);
+    const toggle = () => {
+      encendido.value = !encendido.value;
+    };
     return {
-      text,
+      encendido,
+      toggle,
     };
   },
 };
